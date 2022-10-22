f7c4451ecb7c61eba02f481aaa024071db777f15
Torrin <TPataki1234@gmail.com>
2022-10-22 14:05:26 -0700

    Upload and Download testing

--- src/routes/bigfile.js
+++ src/routes/bigfile.js
@@ -1,8 +1,9 @@
 "use strict";
-var express = require('express');
-var fs = require('fs');
+var express = require("express");
+var fs = require("fs");
 var bigfile = express.Router();
-bigfile.get('/', function (req, res) {
-    var stream = fs.createWriteStream;
+bigfile.get("/", function (req, res) {
+  var src = fs.createReadStream("src/big.file");
+  src.pipe(res);
 });
 module.exports = bigfile;
