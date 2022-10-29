458e3b1b191ece991703523ba94815d3e5b137e4
Torrin <TPataki1234@gmail.com>
2022-10-29 11:46:59 -0700

    c

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -4,8 +4,5 @@ var upload = express.Router();
 var fs = require("fs");
 upload.post('/', function (req, res) {
     console.log(req.body);
-    console.log(req.files);
-    var stream = fs.createReadStream(req.body);
-    stream.pipe(res);
 });
 module.exports = upload;
--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -6,9 +6,6 @@ var fs = require("fs");
 
 upload.post('/', (req: Request, res: Response) => {
     console.log(req.body);
-    console.log(req.files);
-    var stream = fs.createReadStream(req.files);
-    stream.pipe(res);
 })
 
 export = upload
