8e3927454e51e177c5183eb4e5e7e1bfa6b0425d
Torrin <TPataki1234@gmail.com>
2022-10-29 11:04:03 -0700

    Added console logs

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -3,6 +3,7 @@ var express = require('express');
 var upload = express.Router();
 var fs = require("fs");
 upload.post('/', function (req, res) {
+    console.log(req.body);
     var stream = fs.createReadStream(req.body);
     stream.pipe(res);
 });
