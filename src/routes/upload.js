7f9693cc9c6c3e2abe3d7c0e525d9f54fae85944
Torrin <TPataki1234@gmail.com>
2022-11-12 11:30:56 -0800

    y

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -1,13 +1,9 @@
 "use strict";
-var busboy = require("busboy");
 var express = require('express');
 var upload = express.Router();
 var fs = require("fs");
 var cachingUploader = function (cache) {
     return upload.post('/', function (req, res) {
-        if (!fs.existsSync('./uploads/data.csv')) {
-            fs.createFileSync('./uploads/data.csv');
-        }
         fs.appendFileSync("./uploads/data.csv", req.body.data);
         req.body.data.split("\n").forEach(function (row) {
             cache.data.push(row);
--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -6,11 +6,6 @@ var fs = require("fs");
 let cachingUploader = (cache: any) => {
 
   return upload.post('/', (req: Request, res: Response) => {
-
-    if (!fs.existsSync('./uploads/data.csv')) {
-      fs.createFileSync('./uploads/data.csv');
-    }
-
     fs.appendFileSync("./uploads/data.csv", req.body.data);
     req.body.data.split("\n").forEach((row: string) => {
       cache.data.push(row);
